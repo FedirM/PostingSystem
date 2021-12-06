f41bd117b34a98a4fbe20375364d847af73eab3e
FedirM <tedmoster@gmail.com>
2021-12-06 22:55:21 +0200

    TDD: App component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
@@ -7,6 +8,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
 
@@ -16,16 +18,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PostingSystem'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('PostingSystem');
-  });
-
-  it('should render title', () => {
+  it('should title contain "Front-End Code Challenge"', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PostingSystem app is running!');
+    expect(compiled.querySelector('[data-test="navbar-text"]')?.textContent).toContain('Front-End Code Challenge');
   });
 });
